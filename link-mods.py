4016d7fe339b11aecea3d10d9dd6009319676f79
sloe <sloe@sloecoach.com>
2013-04-01 17:06:21 +0100

    Added verbose option to symlink script

--- link-mods.py
+++ link-mods.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 
+import optparse
 import os
 from pprint import pprint
 import sys
+
+parser = optparse.OptionParser()
+parser.add_option("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+(options, args) = parser.parse_args()
 
 toppath = os.path.abspath('..')
 modulespath = os.path.join(toppath, "modules")
 wsmodpath = os.path.join(toppath, "ws-mod")
 gallerymodpath = os.path.join(toppath, "gallery3-contrib", "3.0", "modules")
 
-
-print "toppath=%s" % toppath
-print "modulespath=%s" % modulespath
-print "wsmodpath=%s" % wsmodpath
-print "gallerymodpath=%s" % gallerymodpath
+if options.verbose:
+  print "toppath=%s" % toppath
+  print "modulespath=%s" % modulespath
+  print "wsmodpath=%s" % wsmodpath
+  print "gallerymodpath=%s" % gallerymodpath
 
 if not os.path.isdir(modulespath):
   raise Exception("Missing gallery3 module path %s" % modulespath)
@@ -23,8 +29,6 @@ if not os.path.isdir(wsmodpath):
   
 if not os.path.isdir(gallerymodpath):
   raise Exception("Missing gallery3-contrib checkout module path %s" % gallerymodpath)
-  
-  
 
 module_dirs = {}
 
@@ -44,11 +48,11 @@ for module_name, module_dir in module_dirs.iteritems():
   src_path = os.path.join(modulespath, module_name)
   dest_path = os.path.join(module_dir)
   if os.path.islink(src_path):
-    print "Removing link %s" % src_path
+    if options.verbose: print "Removing link %s" % src_path
     os.remove(src_path)
   if os.path.isfile(src_path):
-    print "Not overriding module directory %s" % src_path
+    if options.verbose: print "Not overriding module directory %s" % src_path
   else:
-    print "Symlinking %s to %s" % (src_path, dest_path)
+    if options.verbose: print "Symlinking %s to %s" % (src_path, dest_path)
     os.symlink(dest_path, src_path)
 
